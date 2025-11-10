c1bf6b4c32975c2fc2e7548869207d8693a097b3
sergioB79 <sergiobatalha79@gmail.com>
2025-11-10 05:32:11 +0000

    Doc novo2

---
+++ gitPush.bat
@@ -0,0 +1,5 @@
+git status               # ver o que mudou
+git add .                # ou: git add ficheiro1 ficheiro2
+git commit -m "Descrição curta e clara"
+git pull --rebase        # traz do remoto e mantém histórico limpo
+git push                 # envia para o GitHub
